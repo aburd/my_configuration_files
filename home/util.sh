9a7848418f78176612ebdcb96016616d188d8a8c
Aaron Burdick <aaron.burdick@protonmail.com>
2022-10-28 16:35:43 +0900

    feat: make connect/disconnect smarter

--- home/util.sh
+++ home/util.sh
@@ -3,18 +3,38 @@
 # Bluetooth
 get_bluetooth_device_id()
 {
-  device_alias=$1
-  bluetoothctl devices | awk '/'"$device_alias"'/ { print $2 }'
+  local device_alias=$1
+  local device_id="$(bluetoothctl devices | awk '/'"$device_alias"'/ { print $2 }')"
+  if [ -z "$device_id" ]; then
+    echo "Bluetooth device not found"
+    return 1
+  fi
+
+  echo $device_id
 }
 
 disconnect_bluetooth()
 {
-  bluetoothctl disconnect "$(get_bluetooth_device_id $1)"
+  is_bluetooth_device_connected $1
+  if [ $? -eq 0 ]; then
+    bluetoothctl disconnect "$(get_bluetooth_device_id $1)"
+    return 0
+  else
+    echo "$1 is already disconnected"
+    return 1
+  fi
 }
 
 connect_bluetooth()
 {
-  bluetoothctl connect "$(get_bluetooth_device_id $1)"
+  is_bluetooth_device_connected $1
+  if [ $? -eq 1 ]; then
+    bluetoothctl connect "$(get_bluetooth_device_id $1)"
+    return 0
+  else
+    echo "$1 is already connected"
+    return 1
+  fi
 }
 
 reset_bluetooth()
@@ -23,3 +43,14 @@ reset_bluetooth()
   connect_bluetooth $1
 }
 
+is_bluetooth_device_connected()
+{
+  device_alias=$1
+  device_id="$(get_bluetooth_device_id "$device_alias")"
+  connected="$(bluetoothctl info "$device_id" | grep Connected | sed -r 's/Connected: (\w+)/\1/' | xargs)"
+  if [ "$connected" = 'yes' ]; then
+    return 0
+  else
+    return 1
+  fi
+}

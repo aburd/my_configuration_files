e2f2cc573157bccc152021942688977b186e5a34
Aaron Burdick <aaron.burdick@protonmail.com>
2022-11-18 13:23:03 +0900

    feat: add volume

--- .config/sway/bar.sh
+++ .config/sway/bar.sh
@@ -9,8 +9,34 @@ bar()
     bat_emoji="🪫"
   fi
   local brightness="$(brightnessctl i | sed -rn 's/.*\(([0-9]{1,3}\%)\).*/\1/p')"
+  local vol="$(amixer sget Master | sed -rn 's/.+\[([0-9]+)%\].+/\1/p')"
+  local v_emoji="$(vol_emoji $vol)"
 
-  echo "💡$brightness $bat_emoji$bat% $d"
+  echo "$v_emoji$vol% 💡$brightness $bat_emoji$bat% $d"
+}
+
+vol_emoji()
+{
+  if is_muted; then
+    echo "🔇"
+  elif [ $1 -gt 70 ]; then
+    echo "🔊"
+  elif [ $1 -gt 35 ]; then
+    echo "🔉"
+  elif [ $1 -gt 0 ]; then
+    echo "🔈"
+  else
+    echo "🔇"
+  fi
+}
+
+is_muted()
+{
+  amixer -D pulse get Master | grep '\[off\]'
+  if [ $? = 0 ]; then
+    return 0
+  fi
+  return 1
 }
 
 bar

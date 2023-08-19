98d45d8c72b9ff4772a77155cdcf4d9a759004db
aburd <aaron.burdick@protonmail.com>
2023-08-19 16:16:22 +0900

    install

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -52,24 +52,43 @@ install_xremap()
   rm "$zip_file"
 }
 
-MY_DEPS="$(cat ./scripts/my_deps.txt | sed -r '/#|^\s*$/d')"
-
-emphasize "This script will attempt to get you up and running with your dependencies"
-emphasize "You can update dependencies at $(pwd)/scripts/my_deps.txt" 
-for dep in $MY_DEPS; do
-  install "$dep"
-done
-if bin_exists xremap; then 
-  echo "xremap already exists, skipping"
-else
-  install_xremap
-fi
-# kickoff
-if bin_exists kickoff; then 
-  echo "kickoff already exists, skipping"
-else
-  cargo install kickoff
-fi
-
 # OhMyZsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+install_ohmyzsh()
+{
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+install_lazygit()
+{
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+}
+
+
+main()
+{
+  MY_DEPS="$(cat ./scripts/my_deps.txt | sed -r '/#|^\s*$/d')"
+  emphasize "This script will attempt to get you up and running with your dependencies"
+  emphasize "You can update dependencies at $(pwd)/scripts/my_deps.txt" 
+  for dep in $MY_DEPS; do
+    install "$dep"
+  done
+  if bin_exists xremap; then 
+    echo "xremap already exists, skipping"
+  else
+    install_xremap
+  fi
+  # kickoff
+  if bin_exists kickoff; then 
+    echo "kickoff already exists, skipping"
+  else
+    cargo install kickoff
+  fi
+
+  install_ohmyzsh
+  install_lazygit
+}
+
+main()

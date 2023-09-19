21b607748a03018bc39804ac8d70ef95d86121df
Aaron Burdick <aaron.burdick@protonmail.com>
2023-09-19 10:09:17 -0400

    feat: add more deps for linux

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -64,12 +64,19 @@ install_lazygit()
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar xf lazygit.tar.gz lazygit
   sudo install lazygit /usr/local/bin
+  rm ./lazygit*
 }
 
 install_wofi_emoji()
 {
   git clone git@github.com:Zeioth/wofi-emoji.git "$HOME/code/wofi-emoji"
   sudo ln -s "$HOME/code/wofi-emoji/wofi-emoji" /usr/local/bin/wofi-emoji
+}
+
+install_vim_plug()
+{
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
 main()
@@ -91,6 +98,11 @@ main()
   ! bin_exists lazygit && install_lazygit || echo "lazygit exists, skipping"
 
   ! bin_exists wofi-emoji && install_wofi_emoji || echo "wofi-emoji exists, skipping"
+
+  if [[ ! -e ~/.vim/autoload/plug.vim ]]; then
+	  echo 'what'
+	  install_vim_plug
+  fi
 }
 
 main

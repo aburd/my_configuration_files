6ed30f277cfe71a38bb31d1c744d30980b58a284
Aaron Burdick <aaron.burdick@protonmail.com>
2024-05-07 00:18:05 +0900

    various configs

--- .config/nvim/configs/fzf.lua
+++ .config/nvim/configs/fzf.lua
@@ -1,5 +1,6 @@
 local telescope = require("telescope")
 local telescopeConfig = require("telescope.config")
+local themes = require("telescope.themes")
 
 -- Clone the default Telescope configuration
 local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
@@ -19,7 +20,9 @@ telescope.setup {
       override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+    },
+    tabs = {
+      theme = themes.get_ivy()
     }
   },
 	defaults = {

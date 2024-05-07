3599e0bcb0e32b5d0825c8c4e9314e6bd70d8ca8
aburd <aaron.burdick@toyokumo.co.jp>
2024-05-07 13:18:46 +0900

    mv fzf to fnl

---
+++ .config/nvim/fnl/configs/fzf.fnl
@@ -0,0 +1,25 @@
+(local telescope (require :telescope))
+(local themes (require :telescope.themes))
+(local telescopeConfig (require :telescope.config))
+
+; Clone the default Telescope configuration)
+(local vimgrep_arguments [(unpack telescopeConfig.values.vimgrep_arguments)])
+
+(table.insert vimgrep_arguments "--hidden")
+(table.insert vimgrep_arguments "--glob")
+(table.insert vimgrep_arguments "!**/.git/*")
+
+(telescope.setup
+  {:extensions {:fzf {:fuzzy true
+                      :override_generic_sorter true
+                      :override_file_sorter true
+                      :case_mode "smart_case"}
+                :tabs {:theme (themes.get_ivy)}}
+   :defaults {: vimgrep_arguments}
+   :pickers {:find_files {:find_command [:rg :--files :--hidden :--glob "!**/.git/*"]}}
+   :buffers {:sort_lastused true
+             :mappings {:i {["c-d>"] "delete_buffer"}}}})
+
+; To get fzf loaded and working with telescope, you need to call)
+; load_extension, somewhere after setup function:)
+((-> (require :telescope) (. :load_extension)) "fzf")
--- .config/nvim/lua/configs/fzf.lua
+++ .config/nvim/lua/configs/fzf.lua
@@ -1,52 +1,10 @@
+-- [nfnl] Compiled from fnl/configs/fzf.fnl by https://github.com/Olical/nfnl, do not edit.
 local telescope = require("telescope")
 local themes = require("telescope.themes")
 local telescopeConfig = require("telescope.config")
-
--- Clone the default Telescope configuration
-local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
-
--- I want to search in hidden/dot files.
+local vimgrep_arguments = {unpack(telescopeConfig.values.vimgrep_arguments)}
 table.insert(vimgrep_arguments, "--hidden")
--- I don't want to search in the `.git` directory.
 table.insert(vimgrep_arguments, "--glob")
 table.insert(vimgrep_arguments, "!**/.git/*")
-
--- You dont need to set any of these options. These are the default ones. Only
--- the loading is important
-telescope.setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    },
-    tabs = {
-      theme = themes.get_ivy()
-    }
-  },
-	defaults = {
-		-- `hidden = true` is not supported in text grep commands.
-		vimgrep_arguments = vimgrep_arguments,
-	},
-	pickers = {
-		find_files = {
-			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-		},
-    buffers = {
-      sort_lastused = true,
-      mappings = {
-        i = {
-          ["<c-d>"] = "delete_buffer",
-        }
-      }
-    }
-	},
-}
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
--- require('telescope').load_extension('tabs')
-
+telescope.setup({extensions = {fzf = {fuzzy = true, override_generic_sorter = true, override_file_sorter = true, case_mode = "smart_case"}, tabs = {theme = themes.get_ivy()}}, defaults = {vimgrep_arguments = vimgrep_arguments}, pickers = {find_files = {find_command = {"rg", "--files", "--hidden", "--glob", "!**/.git/*"}}}, buffers = {sort_lastused = true, mappings = {i = {[{"c-d>"}] = "delete_buffer"}}}})
+return (require("telescope")).load_extension("fzf")
